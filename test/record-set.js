ab63e96eb60ffb990ea585b83a6093faa1361b7f
Paul Frazee <pfrazee@gmail.com>
2017-07-29 12:42:10 -0500

    Finish up query code (with reasonable confidence)

--- test/record-set.js
+++ test/record-set.js
@@ -35,11 +35,153 @@ test.before('setup archives', async () => {
   for (let i = 0; i < 10; i++) {
     archives.push(await def(async write => {
       await write('/single.json', {first: 'first' + i, second: i, third: 'third' + i + 'single'})
-      await write('/multi/1.json', {first: 'first' + i, second: i, third: 'third' + i + 'multi1'})
-      await write('/multi/2.json', {first: 'first' + i, second: i*100, third: 'third' + i + 'multi2'})
-      await write('/multi/3.json', {first: 'first' + i*100, second: i, third: 'third' + i + 'multi3'})
+      await write('/multi/1.json', {first: 'first' + i, second: (i+1)*100, third: 'third' + i + 'multi1'})
+      await write('/multi/2.json', {first: 'first' + i, second: i, third: 'third' + i + 'multi2'})
+      await write('/multi/3.json', {first: 'first' + (i+1)*100, second: i, third: 'third' + i + 'multi3'})
     }))
   }
+})
+
+test('each()', async t => {
+  var n
+  var result
+  const testDB = await setupNewDB()
+  n = 0
+  await testDB.single.getRecordSet().each(result => {
+    n++
+    t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  })
+  t.is(n, 10)
+  n = 0
+  await testDB.multi.getRecordSet().each(result => {
+    n++
+    t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  })
+  t.is(n, 30)
+  await testDB.close()
+})
+
+test('toArray()', async t => {
+  var n
+  var result
+  const testDB = await setupNewDB()
+  n = 0
+  var results = await testDB.single.getRecordSet().toArray()
+  results.forEach(result => {
+    n++
+    t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  })
+  t.is(n, 10)
+  n = 0
+  results = await testDB.multi.getRecordSet().toArray()
+  results.forEach(result => {
+    n++
+    t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  })
+  t.is(n, 30)
+  await testDB.close()
+})
+
+test('eachKey()', async t => {
+  var n
+  var result
+  const testDB = await setupNewDB()
+  n = 0
+  await testDB.single.getRecordSet().eachKey(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is ._url
+    t.truthy(result.startsWith('dat://'))
+    t.truthy(result.endsWith('.json'))
+  })
+  t.is(n, 10)
+  n = 0
+  await testDB.multi.getRecordSet().eachKey(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is .first
+    t.truthy(result.startsWith('first'))
+  })
+  t.is(n, 30)
+  await testDB.close()
+})
+
+test('keys()', async t => {
+  var n
+  var result
+  const testDB = await setupNewDB()
+  n = 0
+  var keys = await testDB.single.getRecordSet().keys()
+  keys.forEach(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is ._url
+    t.truthy(result.startsWith('dat://'))
+    t.truthy(result.endsWith('.json'))
+  })
+  t.is(n, 10)
+  n = 0
+  var keys = await testDB.multi.getRecordSet().keys()
+  keys.forEach(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is .first
+    t.truthy(result.startsWith('first'))
+  })
+  t.is(n, 30)
+  await testDB.close()
+})
+
+test('eachUrl()', async t => {
+  var n
+  var result
+  const testDB = await setupNewDB()
+  n = 0
+  await testDB.single.getRecordSet().eachUrl(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is ._url
+    t.truthy(result.startsWith('dat://'))
+    t.truthy(result.endsWith('.json'))
+  })
+  t.is(n, 10)
+  n = 0
+  await testDB.multi.getRecordSet().eachUrl(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is ._url
+    t.truthy(result.startsWith('dat://'))
+    t.truthy(result.endsWith('.json'))
+  })
+  t.is(n, 30)
+  await testDB.close()
+})
+
+test('urls()', async t => {
+  var n
+  var result
+  const testDB = await setupNewDB()
+  n = 0
+  var urls = await testDB.single.getRecordSet().urls()
+  urls.forEach(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is ._url
+    t.truthy(result.startsWith('dat://'))
+    t.truthy(result.endsWith('.json'))
+  })
+  t.is(n, 10)
+  n = 0
+  urls = await testDB.multi.getRecordSet().urls()
+  urls.forEach(result => {
+    n++
+    t.truthy(typeof result === 'string')
+    // is ._url
+    t.truthy(result.startsWith('dat://'))
+    t.truthy(result.endsWith('.json'))
+  })
+  t.is(n, 30)
+  await testDB.close()
 })
 
 test('first()', async t => {
@@ -49,5 +191,150 @@ test('first()', async t => {
   t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
   result = await testDB.multi.getRecordSet().first()
   t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  await testDB.close()
+})
+
+test('last()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  result = await testDB.single.getRecordSet().last()
+  t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  result = await testDB.multi.getRecordSet().last()
+  t.truthy(result && 'first' in result && 'second' in result && 'third' in result)
+  await testDB.close()
+})
+
+test('count()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  result = await testDB.single.getRecordSet().count()
+  t.is(result, 10)
+  result = await testDB.multi.getRecordSet().count()
+  t.is(result, 30)
+  await testDB.close()
+})
+
+test('orderBy()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  result = await testDB.single.orderBy('first').first()
+  t.is(result.first, 'first0')
+  result = await testDB.single.orderBy('second').first()
+  t.is(result.second, 0)
+  result = await testDB.single.orderBy('first+second').first()
+  t.is(result.first, 'first0')
+  t.is(result.second, 0)
+  result = await testDB.single.orderBy('third').first()
+  t.is(result.third, 'third0single')
+  result = await testDB.multi.orderBy('first').first()
+  t.is(result.first, 'first0')
+  result = await testDB.multi.orderBy('second').first()
+  t.is(result.second, 0)
+  result = await testDB.multi.orderBy('first+second').first()
+  t.is(result.first, 'first0')
+  t.is(result.second, 0)
+  result = await testDB.multi.orderBy('third').first()
+  t.is(result.third, 'third0multi1')
+  await testDB.close()
+})
+
+test('reverse()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  result = await testDB.single.getRecordSet().reverse().toArray()
+  t.truthy(result[0].first, 'first9')
+  result = await testDB.single.orderBy('second').reverse().toArray()
+  t.truthy(result[0].first, 'first9')
+  result = await testDB.multi.getRecordSet().reverse().toArray()
+  t.truthy(result[0].first, 'first9')
+  result = await testDB.multi.orderBy('second').reverse().toArray()
+  t.truthy(result[0].first, 'first9')
+  await testDB.close()
+})
+
+test('distinct()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  result = await testDB.single.getRecordSet().distinct().count()
+  t.is(result, 10)
+  result = await testDB.multi.getRecordSet().distinct().count()
+  t.is(result, 30)
+  result = await testDB.multi.orderBy('second').distinct().count()
+  t.is(result, 20)
+  await testDB.close()
+})
+
+test('uniqueKeys()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  result = await testDB.single.getRecordSet().uniqueKeys()
+  t.is(result.length, 10)
+  result = await testDB.multi.getRecordSet().uniqueKeys()
+  t.is(result.length, 20)
+  await testDB.close()
+})
+
+test('offset() and limit()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.orderBy('first').offset(1).toArray()
+  t.is(results[0].first, 'first1')
+  t.is(results.length, 9)
+  var results = await testDB.single.orderBy('first').limit(2).toArray()
+  t.is(results[0].first, 'first0')
+  t.is(results[1].first, 'first1')
+  t.is(results.length, 2)
+  var results = await testDB.single.orderBy('first').offset(1).limit(2).toArray()
+  t.is(results[0].first, 'first1')
+  t.is(results[1].first, 'first2')
+  t.is(results.length, 2)
+  var results = await testDB.single.orderBy('first').offset(1).limit(2).reverse().toArray()
+  t.is(results[0].first, 'first8')
+  t.is(results[1].first, 'first7')
+  t.is(results.length, 2)
+  await testDB.close()
+})
+
+
+test('filter()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.filter(r => r.first === 'first5').toArray()
+  t.is(results[0].first, 'first5')
+  t.is(results.length, 1)
+  await testDB.close()
+})
+
+test('until()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.orderBy('first').until(r => r.first === 'first5').toArray()
+  t.is(results[0].first, 'first0')
+  t.is(results[5].first, 'first5')
+  t.is(results.length, 6)
+  await testDB.close()
+})
+
+test('or()', async t => {
+  // TODO
+  t.pass()
+})
+
+test('update()', async t => {
+  // TODO
+  t.pass()
+})
+
+test('delete()', async t => {
+  // TODO
+  t.pass()
+})
+
+test('clone()', async t => {
+  var result
+  const testDB = await setupNewDB()
+  var resultSet = testDB.single.orderBy('first')
+  var resultSetClone = resultSet.clone().reverse()
+  result = await resultSet.first()
+  t.is(result.first, 'first0')
+  result = await resultSetClone.first()
+  t.is(result.first, 'first9')
   await testDB.close()
 })
---
+++ test/where-clause.js
@@ -0,0 +1,274 @@
+const test = require('ava')
+const {newDB, ts} = require('./lib/util')
+const IDB = require('../lib/idb-wrapper')
+const DatArchive = require('node-dat-archive')
+const tempy = require('tempy')
+
+var archives = []
+
+async function setupNewDB () {
+  const testDB = newDB()
+  testDB.schema({
+    version: 1,
+    single: {
+      singular: true,
+      index: ['first', 'second', 'first+second', 'third']
+    },
+    multi: {
+      primaryKey: 'first',
+      index: ['first', 'second', 'first+second', 'third']
+    }
+  })
+  await testDB.open()
+  await testDB.addArchives(archives)
+  return testDB
+}
+
+test.before('setup archives', async () => {
+  async function def (fn) {
+    const a = await DatArchive.create({localPath: tempy.directory()})
+    await a.mkdir('/multi')
+    const write = (path, record) => a.writeFile(path, JSON.stringify(record))
+    await fn(write, a)
+    return a
+  }
+  for (let i = 0; i < 10; i++) {
+    archives.push(await def(async write => {
+      await write('/single.json', {first: 'first' + i, second: i, third: 'third' + i + 'single'})
+      await write('/multi/1.json', {first: 'first' + i, second: (i+1)*100, third: 'third' + i + 'multi1'})
+      await write('/multi/2.json', {first: 'first' + i, second: i, third: 'third' + i + 'multi2'})
+      await write('/multi/3.json', {first: 'first' + (i+1)*100, second: i, third: 'third' + i + 'multi3'})
+    }))
+  }
+})
+
+test('above()', async t => {
+  const testDB = await setupNewDB()
+  var result = await testDB.single.where('first').above('first2').first()
+  t.is(result.first, 'first3')
+  var result = await testDB.single.where('second').above(3).first()
+  t.is(result.second, 4)
+  var result = await testDB.single.where('first+second').above(['first5',5]).first()
+  t.is(result.first, 'first6')
+  t.is(result.second, 6)
+  var result = await testDB.single.where('first+second').above(['first5',0]).first()
+  t.is(result.first, 'first5')
+  t.is(result.second, 5)
+  var result = await testDB.single.where('first+second').above(['first5',6]).first()
+  t.is(result.first, 'first6')
+  t.is(result.second, 6)
+  await testDB.close()
+})
+
+test('aboveOrEqual()', async t => {
+  const testDB = await setupNewDB()
+  var result = await testDB.single.where('first').aboveOrEqual('first2').first()
+  t.is(result.first, 'first2')
+  var result = await testDB.single.where('second').aboveOrEqual(3).first()
+  t.is(result.second, 3)
+  var result = await testDB.single.where('first+second').aboveOrEqual(['first5',5]).first()
+  t.is(result.first, 'first5')
+  t.is(result.second, 5)
+  var result = await testDB.single.where('first+second').aboveOrEqual(['first5',0]).first()
+  t.is(result.first, 'first5')
+  t.is(result.second, 5)
+  var result = await testDB.single.where('first+second').aboveOrEqual(['first5',6]).first()
+  t.is(result.first, 'first6')
+  t.is(result.second, 6)
+  await testDB.close()
+})
+
+test('anyOf()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').anyOf('first2', 'first4', 'first6').toArray()
+  t.is(results.length, 3)
+  t.is(results[0].first, 'first2')
+  t.is(results[1].first, 'first4')
+  t.is(results[2].first, 'first6')
+  var results = await testDB.single.where('second').anyOf(2, 4, 6).toArray()
+  t.is(results.length, 3)
+  t.is(results[0].second, 2)
+  t.is(results[1].second, 4)
+  t.is(results[2].second, 6)
+  var results = await testDB.single.where('first').anyOf('first0', 'first10000').toArray()
+  t.is(results.length, 1)
+  t.is(results[0].first, 'first0')
+  await testDB.close()
+})
+
+test('anyOfIgnoreCase()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').anyOfIgnoreCase('FIRST2', 'FIRST4', 'FIRST6').toArray()
+  t.is(results.length, 3)
+  t.is(results[0].first, 'first2')
+  t.is(results[1].first, 'first4')
+  t.is(results[2].first, 'first6')
+  var results = await testDB.single.where('second').anyOfIgnoreCase(2, 4, 6).toArray()
+  t.is(results.length, 3)
+  t.is(results[0].second, 2)
+  t.is(results[1].second, 4)
+  t.is(results[2].second, 6)
+  var results = await testDB.single.where('first').anyOfIgnoreCase('FIRST0', 'FIRST10000').toArray()
+  t.is(results.length, 1)
+  t.is(results[0].first, 'first0')
+  await testDB.close()
+})
+
+test('below()', async t => {
+  const testDB = await setupNewDB()
+  var result = await testDB.single.where('first').below('first2').last()
+  t.is(result.first, 'first1')
+  var result = await testDB.single.where('second').below(3).last()
+  t.is(result.second, 2)
+  var result = await testDB.single.where('first+second').below(['first5',5]).last()
+  t.is(result.first, 'first4')
+  t.is(result.second, 4)
+  var result = await testDB.single.where('first+second').below(['first5',0]).last()
+  t.is(result.first, 'first4')
+  t.is(result.second, 4)
+  var result = await testDB.single.where('first+second').below(['first5',6]).last()
+  t.is(result.first, 'first5')
+  t.is(result.second, 5)
+  await testDB.close()
+})
+
+test('belowOrEqual()', async t => {
+  const testDB = await setupNewDB()
+  var result = await testDB.single.where('first').belowOrEqual('first2').last()
+  t.is(result.first, 'first2')
+  var result = await testDB.single.where('second').belowOrEqual(3).last()
+  t.is(result.second, 3)
+  var result = await testDB.single.where('first+second').belowOrEqual(['first5',5]).last()
+  t.is(result.first, 'first5')
+  t.is(result.second, 5)
+  var result = await testDB.single.where('first+second').belowOrEqual(['first5',0]).last()
+  t.is(result.first, 'first4')
+  t.is(result.second, 4)
+  var result = await testDB.single.where('first+second').belowOrEqual(['first5',6]).last()
+  t.is(result.first, 'first5')
+  t.is(result.second, 5)
+  await testDB.close()
+})
+
+test('between()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').between('first2', 'first4').toArray()
+  t.is(results.length, 1)
+  t.is(results[0].first, 'first3')
+  var results = await testDB.single.where('first').between('first2', 'first4', {includeLower: true}).toArray()
+  t.is(results.length, 2)
+  t.is(results[0].first, 'first2')
+  t.is(results[1].first, 'first3')
+  var results = await testDB.single.where('first').between('first2', 'first4', {includeUpper: true}).toArray()
+  t.is(results.length, 2)
+  t.is(results[0].first, 'first3')
+  t.is(results[1].first, 'first4')
+  var results = await testDB.single.where('first').between('first2', 'first4', {includeLower: true, includeUpper: true}).toArray()
+  t.is(results.length, 3)
+  t.is(results[0].first, 'first2')
+  t.is(results[1].first, 'first3')
+  t.is(results[2].first, 'first4')
+  var results = await testDB.single.where('second').between(2, 4).toArray()
+  t.is(results.length, 1)
+  t.is(results[0].second, 3)
+  var results = await testDB.single.where('second').between(2, 4, {includeLower: true}).toArray()
+  t.is(results.length, 2)
+  t.is(results[0].second, 2)
+  t.is(results[1].second, 3)
+  var results = await testDB.single.where('second').between(2, 4, {includeUpper: true}).toArray()
+  t.is(results.length, 2)
+  t.is(results[0].second, 3)
+  t.is(results[1].second, 4)
+  var results = await testDB.single.where('second').between(2, 4, {includeLower: true, includeUpper: true}).toArray()
+  t.is(results.length, 3)
+  t.is(results[0].second, 2)
+  t.is(results[1].second, 3)
+  t.is(results[2].second, 4)
+  await testDB.close()
+})
+
+test('equals()', async t => {
+  const testDB = await setupNewDB()
+  var result = await testDB.single.where('first').equals('first2').first()
+  t.is(result.first, 'first2')
+  var result = await testDB.single.where('second').equals(3).first()
+  t.is(result.second, 3)
+  var result = await testDB.single.where('first+second').equals(['first4',4]).first()
+  t.is(result.first, 'first4')
+  t.is(result.second, 4)
+  var result = await testDB.single.where('first').equals('no match').first()
+  t.falsy(result)
+  await testDB.close()
+})
+
+test('equalsIgnoreCase()', async t => {
+  const testDB = await setupNewDB()
+  var result = await testDB.single.where('first').equalsIgnoreCase('FIRST2').first()
+  t.is(result.first, 'first2')
+  var result = await testDB.single.where('second').equalsIgnoreCase(3).first()
+  t.is(result.second, 3)
+  await testDB.close()
+})
+
+test('noneOf()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').noneOf('first2', 'first4', 'first6').toArray()
+  t.is(results.length, 7)
+  var results = await testDB.single.where('second').noneOf(2, 4, 6).toArray()
+  t.is(results.length, 7)
+  var results = await testDB.single.where('first').noneOf('first0', 'first10000').toArray()
+  t.is(results.length, 9)
+  await testDB.close()
+})
+
+test('notEqual()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').notEqual('first2').toArray()
+  t.is(results.length, 9)
+  var results = await testDB.single.where('second').notEqual(2).toArray()
+  t.is(results.length, 9)
+  var results = await testDB.single.where('first').noneOf('first10000').toArray()
+  t.is(results.length, 10)
+  await testDB.close()
+})
+
+test('startsWith()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').startsWith('first').toArray()
+  t.is(results.length, 10)
+  var results = await testDB.single.where('first').startsWith('nomatch').toArray()
+  t.is(results.length, 0)
+  await testDB.close()
+})
+
+test('startsWithAnyOf()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').startsWithAnyOf('first1', 'first2').toArray()
+  t.is(results.length, 2)
+  var results = await testDB.single.where('first').startsWithAnyOf('first', 'nomatch').toArray()
+  t.is(results.length, 10)
+  var results = await testDB.single.where('first').startsWithAnyOf('nomatch').toArray()
+  t.is(results.length, 0)
+  await testDB.close()
+})
+
+test('startsWithAnyOfIgnoreCase()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').startsWithAnyOfIgnoreCase('FIRST1', 'FIRST2').toArray()
+  t.is(results.length, 2)
+  var results = await testDB.single.where('first').startsWithAnyOfIgnoreCase('FIRST', 'NOMATCH').toArray()
+  t.is(results.length, 10)
+  var results = await testDB.single.where('first').startsWithAnyOfIgnoreCase('NOMATCH').toArray()
+  t.is(results.length, 0)
+  await testDB.close()
+})
+
+test('startsWithIgnoreCase()', async t => {
+  const testDB = await setupNewDB()
+  var results = await testDB.single.where('first').startsWithIgnoreCase('FIRST').toArray()
+  t.is(results.length, 10)
+  var results = await testDB.single.where('first').startsWithIgnoreCase('NOMATCH').toArray()
+  t.is(results.length, 0)
+  await testDB.close()
+})
+
