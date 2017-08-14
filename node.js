1b256609b77c42d3d9e42a3cde01de158a7b0a19
Paul Frazee <pfrazee@gmail.com>
2017-08-14 16:54:56 -0500

    Fixes for node/browser behaviors

--- node.js
+++ node.js
@@ -1,17 +1,11 @@
 const DatArchive = require('node-dat-archive')
 const tempy = require('tempy')
 
-class NodeDatArchive extends DatArchive {
-  constructor(url) {
-    super(url, {localPath: tempy.directory()})
-  }
-}
-
 global.window = {
   localStorage: {
     LOG_LEVEL: process.env.LOG_LEVEL
   },
-  DatArchive: NodeDatArchive
+  DatArchive
 }
 
 module.exports = require('./index')
